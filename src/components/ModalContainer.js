de7878a01c99d56675be4837103bc82eaf10bf29
Casey Khuc <khuctrang1812@gmail.com>
2019-12-16 17:21:58 +0700

    init modal

--- src/components/ModalContainer.js
+++ src/components/ModalContainer.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import modalKeys from '../constants/modelKeys';
+import RegisterModal from './Modal/RegisterModal';
+import SignInModal from './Modal/SignInModal';
 
 const ModalContainer = ({ modal }) => {
-  return <div>ModalContainer {modal.current}</div>;
+  const renderModal = () => {
+    switch (modal.current) {
+      case modalKeys.REGISTER_MODAL:
+        return <RegisterModal />;
+      case modalKeys.SIGNIN_MODAL:
+        return <SignInModal />;
+      default:
+        return '';
+    }
+  };
+
+  //return <div>ModalContainer {modal.current}</div>;
+  return <div>{renderModal()}</div>;
 };
 
 const mapStateToProps = (state) => {
