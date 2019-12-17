b3f38c55a408de975dd8ccf198867347a16df0df
Casey Khuc <khuctrang1812@gmail.com>
2019-12-17 10:34:11 +0700

    add eslint airbnb config

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,14 +5,17 @@ import user from './user';
 import modal from './modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const reducer = combineReducers({ user, item, category, modal });
+const reducer = combineReducers({
+  user,
+  item,
+  category,
+  modal,
+});
 
 export default reducer;
 
-export const getCategories = (state) =>
-  fromCategory.getCategories(state.category);
+export const getCategories = (state) => fromCategory.getCategories(state.category);
 
-export const getCategoryIds = (state) =>
-  fromCategory.getCategoryIds(state.category);
+export const getCategoryIds = (state) => fromCategory.getCategoryIds(state.category);
 
 export const getItems = (state) => fromItem.getItems(state.item);
