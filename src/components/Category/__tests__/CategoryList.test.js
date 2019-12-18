be9fb686e50a6b40f33ac316cb2e56b89c09c01e
Casey Khuc <khuctrang1812@gmail.com>
2019-12-18 12:22:03 +0700

    refactor category test"

--- src/components/Category/__tests__/CategoryList.test.js
+++ src/components/Category/__tests__/CategoryList.test.js
@@ -1,10 +1,7 @@
 
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import CategoryList from '../CategoryList';
-
-configure({ adapter: new Adapter() })
 
 describe('component/Category/CategoryList', () => {
   let props;
