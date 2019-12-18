0772a8293deccfae9777ffec8cd86079d216d242
Casey Khuc <khuctrang1812@gmail.com>
2019-12-18 20:34:43 +0700

    refactor: Config

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
-import { baseURL, ITEM_PER_PAGE } from './config';
+import { BASE_URL, ITEM_PER_PAGE } from '../config';
 
-export const request = (path, options = {}) => fetch(`${baseURL}${path}`, { ...options });
-export const fetchCategories = () => fetch(`${baseURL}/categories?offset=0&limit=100`)
+export const request = (path, options = {}) => fetch(`${BASE_URL}${path}`, { ...options });
+export const fetchCategories = () => fetch(`${BASE_URL}/categories?offset=0&limit=100`)
   .then((res) => {
     if (res.ok) { return res.json(); }
     throw res.json();
   })
   .then((res) => res.categories);
 
-export const fetchItems = (categoryId, page) => fetch(`${baseURL}/categories/${categoryId}/items?offset=${page * ITEM_PER_PAGE}&limit=${ITEM_PER_PAGE}`)
+export const fetchItems = (categoryId, page) => fetch(`${BASE_URL}/categories/${categoryId}/items?offset=${page * ITEM_PER_PAGE}&limit=${ITEM_PER_PAGE}`)
   .then((res) => {
     if (res.ok) { return res.json(); }
     throw res.json();
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 // will change to using ENVIRONMENT VARIABLE
-export const baseURL = 'http://localhost:5000';
+export const BASE_URL = 'http://localhost:5000';
 export const ITEM_PER_PAGE = 10;
