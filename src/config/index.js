5b8b41fcb360a00d5a39d20acb51f0dc4f57be78
Casey Khuc <khuctrang1812@gmail.com>
2019-12-24 09:52:46 +0700

    refactor: Multiple env config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,21 @@
-// will change to using ENVIRONMENT VARIABLE
-export const BASE_URL = 'http://localhost:5000';
-export const ITEM_PER_PAGE = 1;
+import * as fromDev from './dev';
+import * as fromLocal from './local';
+import * as fromProd from './prod';
+import * as fromStaging from './staging';
+
+const config = () => {
+  switch (process.env.REACT_APP_ENV) {
+    case 'dev':
+      return fromDev;
+    case 'local':
+      return fromLocal;
+    case 'prod':
+      return fromProd;
+    case 'staging':
+      return fromStaging;
+    default:
+      return fromDev;
+  }
+}
+
+export default { ...config() };
