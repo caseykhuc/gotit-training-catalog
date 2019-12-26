ffeaecc815028e4879ae4ffee157236855df32ff
Casey Khuc <khuctrang1812@gmail.com>
2019-12-26 19:01:27 +0700

    test: NotFoundPage

---
+++ src/components/common/__tests__/NotFoundPage.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotFoundPage from '../NotFoundPage';
+
+describe('component/NotFoundPage', () => {
+  let props;
+  let wrapper;
+
+  const update = () => {
+    wrapper.update();
+  };
+  const setup = () => {
+    wrapper = shallow(<NotFoundPage {...props} />);
+    update();
+  };
+  beforeEach(() => {
+    props = {
+    }
+  });
+  it('should render correctly', () => {
+    setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+})
